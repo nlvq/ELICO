70c9d647ce96af1ca5a71ed0d6b375a0b8a06343
Safwen <khalloufi.safwen@gmail.com>
2013-01-28 23:18:08 +0100

    rh

--- commun/src/main/java/coeur_metier/rh/IRH.java
+++ commun/src/main/java/coeur_metier/rh/IRH.java
@@ -1,5 +1,20 @@
 package coeur_metier.rh;
 
+import java.util.List;
+
+import dao.Organisation;
+import dao.Role;
+import dao.Utilisateur;
+
 public interface IRH {
+
+	public void createOrga(String name, String type, Organisation parentOrga);
+	public void deleteOrga(String name);
+	public List<Organisation> findOrga(String Orga);
+	public void createUser(String user, String nameOrga);
+	public void deleteUser(String loginUser) ;
+	public void setRoles(String loginUser, List<Role> role);
+	public List<Utilisateur> findUser(String loginUser);
+	public void resetPassword(String loginUsern);
 
 }
