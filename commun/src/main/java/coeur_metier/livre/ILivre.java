6bb94ae88a0f9269eec9133359a64d183686ba68
ychikhi <yasminachikhi@hotmail.com>
2013-01-28 22:14:13 +0100

    Update commun/src/main/java/coeur_metier/livre/ILivre.java

--- commun/src/main/java/coeur_metier/livre/ILivre.java
+++ commun/src/main/java/coeur_metier/livre/ILivre.java
@@ -1,5 +1,18 @@
 package coeur_metier.livre;
+import java.util.List;
+import dao.Objet;
 
 public interface ILivre {
+public void CreateObject(Objet  object) {  }
+public void CreateChapter( String IdVolPere ){}
+public void CreateVolume( String IdVolLivre ){}
+public void CreateParagraph( String IdChapPere ){}
+public void UpdateObject(Objet object) {}
+public void EditObject(Objet object) {}
+public void DeleteObject(Objet object) {}
+public List<Objet > FindObject(Objet object) {
+List <Objet > list=NULL;
+  return (list);
+  }
 
 }
