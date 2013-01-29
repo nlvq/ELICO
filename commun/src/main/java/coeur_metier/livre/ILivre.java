c9354e0e1e95ec178d2e8faf16b93c2243c9bac3
Giry Geoffrey <ggiry@etudiant.univ-mlv.fr>
2013-01-29 18:38:35 +0100

    update git ignore

--- commun/src/main/java/coeur_metier/livre/ILivre.java
+++ commun/src/main/java/coeur_metier/livre/ILivre.java
@@ -3,18 +3,8 @@ import java.util.List;
 import dao.Objet;
 
 public interface ILivre {
-<<<<<<< HEAD
-<<<<<<< HEAD
     /*
     public void CreateObject(Objet  object) {  }
-=======
-/*
-	public void CreateObject(Objet  object) {  }
->>>>>>> e7dae33a400c0c957c0594737b2e28d993ccdc73
-=======
-/*
-	public void CreateObject(Objet  object) {  }
->>>>>>> 3d0e797cabdb84467ae4552fe1f4eb29b389eaa3
 public void CreateChapter( String IdVolPere ){}
 public void CreateVolume( String IdVolLivre ){}
 public void CreateParagraph( String IdChapPere ){}
@@ -25,9 +15,5 @@ public List<Objet > FindObject(Objet object) {
 List <Objet > list=NULL;
   return (list);
   }
-*/
-<<<<<<< HEAD
+
 }
-=======
-}
->>>>>>> 3d0e797cabdb84467ae4552fe1f4eb29b389eaa3
